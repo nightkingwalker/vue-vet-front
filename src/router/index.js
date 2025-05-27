80c6571f970dc7ee14365d8c8cdd5d20f9405f84
Tarek Sawah <t.sawah@gmail.com>
2025-05-27 16:50:29 +0300

    Lower Build size

--- src/router/index.js
+++ src/router/index.js
@@ -1,40 +1,34 @@
 // src/router/index.js
 import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '../views/CalendarView.vue';
-import LoginView from '../components/LoginComponent.vue';
-import OwnerPets from '../views/OwnerPets.List.vue';
-import OwnersList from '../views/OwnersList.vue';
-// import OwnersList2 from '../views/OwnersList2.vue';
-import Reports from '@/views/Reports.vue'
 import { useAuthStore } from '@/stores/authStore';
-import ManageClinic from '@/views/ManageClinic.vue';
-import ListClinicBranches from '@/views/ListClinicBranches.vue';
-import ManageBranch from '@/views/ManageBranch.vue';
-
 const router = createRouter({
     history: createWebHistory(),
     routes: [{
         path: '/login',
         name: 'Login',
-        component: LoginView,
+        // component: LoginView,
+        component: () =>
+            import('@/components/LoginComponent.vue'),
         meta: { allowSessionTimeout: false }
     },
     {
         path: '/account',
         name: 'Account',
         component: () =>
-            import('../views/AboutView.vue')
+            import('@/views/AboutView.vue')
     },
     {
         path: '/',
         name: 'Home',
-        component: HomeView,
+        component: () =>
+            import('@/views/CalendarView.vue'),
         meta: { requiresAuth: true, allowSessionTimeout: true }
     },
     {
         path: '/owners',
         name: 'Owners',
-        component: OwnersList,
+        component: () =>
+            import('@/views/OwnersList.vue'),
         meta: { allowSessionTimeout: true }
     },
 
@@ -42,8 +36,8 @@ const router = createRouter({
         path: '/:ownerid/pets',
         name: 'OwnerPets',
         meta: { transition: 'slide-right' },
-        component: OwnerPets,
-
+        component: () =>
+            import('@/views/OwnerPets.List.vue'),
         props: true, // This passes the route params as props to the component,
         meta: { allowSessionTimeout: true }
     },
@@ -51,7 +45,7 @@ const router = createRouter({
         path: '/new-owner',
         name: 'NewOwner',
         component: () =>
-            import('../views/AddNewOwner.vue'),
+            import('@/views/AddNewOwner.vue'),
 
         props: true, // This passes the route params as props to the component,
         meta: { allowSessionTimeout: true }
@@ -60,15 +54,15 @@ const router = createRouter({
             path: '/clinics',
             name: 'Clinics',
             component: () =>
-                import('../views/ListUserClinics.vue'),
+                import('@/views/ListUserClinics.vue'),
             meta: { allowSessionTimeout: true }
         },
         {
             path: '/:clinic/manage',
             name: 'Manage Clinic',
             meta: { transition: 'slide-right' },
-            component: ManageClinic,
-
+            component: () =>
+                import('@/views/ManageClinic.vue'),
             props: true, // This passes the route params as props to the component,
             meta: { allowSessionTimeout: true }
         },
@@ -76,8 +70,8 @@ const router = createRouter({
             path: '/clinics/:clinic/branches',
             name: 'Manage Clinic Branches',
             meta: { transition: 'slide-right' },
-            component: ListClinicBranches,
-
+            component: () =>
+                import('@/views/ListClinicBranches.vue'),
             props: true, // This passes the route params as props to the component,
             meta: { allowSessionTimeout: true }
         },
@@ -85,8 +79,8 @@ const router = createRouter({
             path: '/branches/:branch/users',
             name: 'Branch Users',
             meta: { transition: 'slide-right' },
-            component: ManageBranch,
-
+            component: () =>
+                import('@/views/ManageBranch.vue'),
             props: true, // This passes the route params as props to the component,
             meta: { allowSessionTimeout: true }
         },
@@ -95,90 +89,91 @@ const router = createRouter({
         path: '/pets',
         name: 'Pets',
         component: () =>
-            import('../views/PetsList.vue'),
+            import('@/views/PetsList.vue'),
         meta: { allowSessionTimeout: true }
     },
     {
         path: '/pets/:petmicrochip',
         name: 'PetDetails',
         component: () =>
-            import('../views/PetDetails.vue'),
+            import('@/views/PetDetails.vue'),
         meta: { allowSessionTimeout: true }
     }, {
         path: '/new-pet',
         name: 'AddNewPet',
         component: () =>
-            import('../views/AddNewPet.vue'),
+            import('@/views/AddNewPet.vue'),
         meta: { allowSessionTimeout: true }
     },
     {
         path: '/about',
         name: 'About',
         component: () =>
-            import('../views/AboutView.vue'),
+            import('@/views/AboutView.vue'),
         meta: { allowSessionTimeout: true }
     },
     {
         path: '/profile',
         name: 'Profile',
         component: () =>
-            import('../views/UserProfile.vue'),
+            import('@/views/UserProfile.vue'),
         meta: { allowSessionTimeout: true }
     },
     {
         path: '/add-inventory-food',
         name: 'New Inventory',
         component: () =>
-            import('../views/InventoryItemsList.vue'),
+            import('@/views/InventoryItemsList.vue'),
         meta: { allowSessionTimeout: true }
     },
     {
         path: '/add-inventory-toys',
         name: 'New Toys Inventory',
         component: () =>
-            import('../views/InventoryItemsList.vue'),
+            import('@/views/InventoryItemsList.vue'),
         meta: { allowSessionTimeout: true }
     },
     {
         path: '/add-inventory-pharmaceutical',
         name: 'New Pharmaceutical Inventory',
         component: () =>
-            import('../views/InventoryItemsList.vue'),
+            import('@/views/InventoryItemsList.vue'),
         meta: { allowSessionTimeout: true }
     },
     {
         path: '/add-inventory-grooming',
         name: 'New Grooming Inventory',
         component: () =>
-            import('../views/InventoryItemsList.vue'),
+            import('@/views/InventoryItemsList.vue'),
         meta: { allowSessionTimeout: true }
     },
     {
         path: '/add-inventory-treatment',
         name: 'New Treatment Inventory',
         component: () =>
-            import('../views/InventoryItemsList.vue'),
+            import('@/views/InventoryItemsList.vue'),
         meta: { allowSessionTimeout: true }
     },
     {
         path: '/invoices',
         name: 'Invoice List',
         component: () =>
-            import('../views/InvoiceList.vue'),
+            import('@/views/InvoiceList.vue'),
         meta: { allowSessionTimeout: true }
     },
     {
         path: '/settings',
         name: 'Settings',
         component: () =>
-            import('../views/WhatsAppOnboardingLanding.vue'),
-        // import('../views/Settings.vue'),
+            import('@/views/WhatsAppOnboardingLanding.vue'),
+        // import('@/views/Settings.vue'),
         meta: { allowSessionTimeout: true }
     },
         {
             path: '/reset-password',
             name: 'reset-password',
-            component: LoginView, // Reuse your login component
+            component: () =>
+                import('@/components/LoginComponent.vue'),
             props: (route) => ({
                 action: 'reset',
                 code: route.query.code
@@ -188,12 +183,14 @@ const router = createRouter({
         //     path: '/shortcuts',
         //     name: 'Shortcuts',
         //     component: () =>
-        //         import('../views/Shortcuts.vue'),
+        //         import('@/views/Shortcuts.vue'),
         //     meta: { allowSessionTimeout: true }
         // },
     {
         path: '/reports',
-        component: Reports, // Your current component
+        component: () =>
+            import('@/views/Reports.vue'),
+        // component: Reports, // Your current component
         children: [
             {
                 path: 'stock-movement', component: () => import('@/views/partials/Reports/StockMovement.vue'),
@@ -210,9 +207,9 @@ const router = createRouter({
         ]
     },
     {
-            path: '/whatsapp/onboarding',
-            name: 'whatsapp-onboarding',
-            component: import('../views/WhatsAppOnboardingLanding.vue'),
+        path: '/whatsapp/onboarding',
+        name: 'whatsapp-onboarding',
+        component: () => import('@/views/WhatsAppOnboardingLanding.vue'),
             meta: {
                 requiresAuth: true,
                 breadcrumb: 'WhatsApp Setup',
@@ -222,7 +219,9 @@ const router = createRouter({
         {
             path: '/whatsapp/onboarding/number-verification',
             name: 'whatsapp-number-verification',
-            component: import('../views/WhatsAppNumberVerification.vue'),
+            // component: () => import('@/views/WhatsAppNumberVerification.vue'),
+            component: () => import('@/views/WahaNumVerifiy.vue'),
+            // component: WhatsAppNumberVerification,
             meta: {
                 requiresAuth: true,
                 breadcrumb: 'Number Verification',
@@ -232,7 +231,7 @@ const router = createRouter({
         {
             path: '/whatsapp/onboarding/qr-scan',
             name: 'whatsapp-qr-scanner',
-            component: import('../views/WhatsAppQRScanner.vue'),
+            component: () => import('@/views/WhatsAppQRScanner.vue'),
             meta: {
                 requiresAuth: true,
                 breadcrumb: 'QR Connection',
@@ -242,17 +241,7 @@ const router = createRouter({
                 phone: route.query.phone
             })
         },
-    /* {
-        path: '/whatsapp/onboarding/success',
-        name: 'whatsapp-connection-success',
-        component: WhatsAppConnectionSuccess,
-        meta: {
-            requiresAuth: true,
-            breadcrumb: 'Setup Complete',
-            feature: 'whatsapp_integration'
-        }
-    },
- */    {
+        {
         path: '/logout',
         name: 'Logout',
     },
